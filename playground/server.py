d190eae525b3bf422eaacd662240f94a72e10e58
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-09-03 15:00:16 -0500

    yayay

--- playground/server.py
+++ playground/server.py
@@ -3,13 +3,24 @@ app = Flask(__name__)
 
 @app.route('/')        
 def index():
-    return render_template("index.html")
+    return render_template("index.html", blocks=0)
+
+@app.route('/play')         
+def play():
+    return render_template("index.html", blocks=3)
 
 @app.route('/play/<int:blocks>')         
-def blocks(blocks):
-    return '${blocks}'
+def playcustom(blocks):
+    return render_template("index.html", blocks=blocks)
 
+@app.route('/play/<int:blocks>/<string:color>')         
+def color(color):
+    return render_template("index.html", blocks="color")
 
 @app.route('/<other>')
 def sorry(other):
     return f"Sorry! No response. Try Again"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
