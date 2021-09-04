0af35c47cecd1a5f8a17b5bd74acd97e0340be0e
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-09-04 13:04:41 -0500

    check

---
+++ checkerboard/server.py
@@ -0,0 +1,23 @@
+from flask import Flask , render_template 
+app = Flask(__name__)    
+
+@app.route('/')        
+def index():
+    return render_template("index.html", y=8, x=8)
+
+@app.route('/<int:y>')         
+def y(y):
+    return render_template("index.html", y=y, x=8)
+
+@app.route('/<int:y>/<int:x>')         
+def yandx(y, x):
+    return render_template("index.html", y=y, x=x)
+
+
+@app.route('/<other>')
+def sorry(other):
+    return f"Sorry! No response. Try Again"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
